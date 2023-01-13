5e5ab33e680be9a2198e344d7a0e6030167e1086
Osorgin Ivan <igosorgin@edu.hse.ru>
2023-01-13 21:30:46 +0300

    successfuly went through all files not saving in db

--- parser/main.py
+++ parser/main.py
@@ -1,9 +1,18 @@
-from src.parser import parsed
+from src.parser import parsed_translations
 from pprint import pprint
+from time import time
+
+from tqdm import tqdm
 
 def main():
-    for p in parsed('paralleltext-master/bibles/corpus/'):
-        pprint(p)
+    pb = tqdm(unit=" lines", colour="pink", unit_scale=True)
+    file_count = 0
+    for p in parsed_translations('paralleltext-master/bibles/corpus/'):
+        file_count += 1
+        pb.update(len(p["data"]["lines"]))
+        if file_count % 25 == 0: pb.set_postfix({'Files': file_count})
     
 if __name__ == '__main__':
+    b_t = time()
     main()
+    print(f"It took {round(time() - b_t, 2)}s")
--- parser/src/parser.py
+++ parser/src/parser.py
@@ -1,8 +1,9 @@
 from os import listdir as os_listdir
 from pathlib import Path
-from re import match as regex_match
-
+from re import match as regex_match, search as regex_search
 from pprint import pprint
+
+from .line_blacklist import blacklist
 
 def __get_files(rel_path) -> list[Path]:
     """ Get all file names from inputed relative dir path. (Relative to 'main.py')"""
@@ -35,12 +36,24 @@ def __get_file_lines(file_name: str) -> str:
                 continue
             yield line
 
-def parse_file(file_name: str, empty_value = None):
+def parse_file(file_name: str, empty_value = None) -> dict[str, dict]:
     """ TODO """
     
     meta = {}
-    data = {}
-    for l in __get_file_meta(file_name):
+    data = {
+        "lines": []
+    }
+    line_no = 0
+    
+    def check_blacklist(l):
+        if l.strip() in blacklist:
+            print(f"\nMet balcklisted line in {file_name} line: {line_no}\n{l}\n")
+            return False
+        return True
+    
+    for l in __get_file_meta(file_name=file_name):
+        if not check_blacklist(l): continue
+        line_no += 1
         try:
             key, val = l.replace('# ', '').split(':', maxsplit=1)
         except ValueError:
@@ -51,13 +64,33 @@ def parse_file(file_name: str, empty_value = None):
         key = key.strip().lower(); val = val.strip()
         if val == '': val = empty_value
         meta[key] = val
+        
+    for l in __get_file_lines(file_name=file_name):
+        if not check_blacklist(l): continue
+        line_no += 1
+        try:
+            b_id, ch_id, vrs_id, line = regex_search(r'^(\d{2})(\d{3})(\d{3})\t(.*)$', l).groups()
+        except AttributeError:
+            try:
+                b_id, ch_id, vrs_id = regex_search(r'^(\d{2})(\d{3})(\d{3})', l).groups()
+            except AttributeError:
+                raise AttributeError(f'Regex failed at line: {l.strip()}\nFile: {file_name}\nLine: {line_no}')
+            finally:
+                line = empty_value
+        data["lines"].append({
+            "book_id": b_id,
+            "chapter_id": ch_id,
+            "verse_id": vrs_id,
+            "line": line
+        })
+        
     return {
         'file_name': file_name,
         'meta': meta,
         'data': data
     }
 
-def parsed(rel_path):
+def parsed_translations(rel_path):
     files = __get_files(rel_path)
     for f in files:
         yield parse_file(f)

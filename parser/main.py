5762387adbfb2c6733e6d6a94f62e75c24b77f5c
Ivan Osorgin <affenmilchmann@gmain.com>
2023-01-17 20:56:01 +0300

    temp json files

--- parser/main.py
+++ parser/main.py
@@ -5,10 +5,17 @@ from tqdm import tqdm
 from src.parser import parsed_translations
 from src.db_handler import write_translation
 
+def temp_json_filler(np_dict: dict):
+    from json import dump
+    with open("bible_json/" + np_dict["file_name"].replace(".txt", ".json"), 'w', encoding='utf-8') as f:
+        dump(np_dict, f, indent=2)
+
 def main():
     pb = tqdm(unit=" lines", colour="pink", unit_scale=True)
     file_count = 0
     for p in parsed_translations('paralleltext-master/bibles/corpus/'):
+        if file_count > 2: break
+        temp_json_filler(p)
         file_count += 1
         pb.update(len(p["data"]["lines"]))
         if file_count % 25 == 0: pb.set_postfix({'Files': file_count})

d9c6532edf8eecf5d4256461a12121288fae857a
Ivan Osorgin <affenmilchmann@gmain.com>
2023-01-24 17:44:33 +0300

    merge with bible_parser

--- parser/src/db_handler.py
+++ parser/src/db_handler.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 import logging
-import psycopg2
+import psycopg2 as db
 
 logging.basicConfig(format='  [%(levelname)s]:\t%(message)s', level=logging.DEBUG)
 
@@ -32,18 +32,13 @@ TABLES = {
         data                TEXT,
         PRIMARY KEY(segment_id, translation_id)
         """,
-    "hashed_wordforms":
-        """
-        wordform_hash       SERIAL PRIMARY KEY,
-        segment_id          INT,
-        translation_id      INT
-        """,
 }
+
 DB_NAME = "parabible"
 HOST_ADDR = "0.0.0.0"
 PORT = "5432"
 
-conn = psycopg2.connect(
+conn = db.connect(
         database = DB_NAME,
         user = "dev",
         password = "dev",
@@ -51,7 +46,31 @@ conn = psycopg2.connect(
         port = PORT
     )
 
-def write_translation(translation: dict[dict, str]):
+def write_text_meta(meta: dict):
+    cursor = conn.cursor()
+    cursor.execute("""
+            INSERT INTO translations
+            (
+                lang_id, closest_iso_639_3, iso_15924,
+                year_short, year_long,
+                vernacular_title, english_title, url,
+                copyright_short, copyright_long, notes
+            ) VALUES
+            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            RETURNING id
+        """,
+        (
+            2, meta["closest iso 639-3"], meta["iso_15924"],
+            meta["year_short"], meta["year_long"],
+            meta["vernacular_title"], meta["english_title"], meta["url"],
+            meta["copyright_short"], meta["copyright_long"], meta["notes"]
+        )
+    )
+    conn.commit()
+    returned_id = cursor.fetchone()
+    logging.debug(f"Inserted id: {returned_id}")
+
+def write_text_data(data: list[dict]):
     pass
 
 def init_db():

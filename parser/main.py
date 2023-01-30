ce66849f0729e1d6a8a9481cbe9176eebfcde01d
Ivan Osorgin <affenmilchmann@gmain.com>
2023-01-30 12:25:13 +0300

    more parsed

--- parser/main.py
+++ parser/main.py
@@ -14,7 +14,6 @@ def main():
     pb = tqdm(unit=" lines", colour="pink", unit_scale=True)
     file_count = 0
     for p in parsed_translations('paralleltext-master/bibles/corpus/'):
-        if file_count > 2: break
         temp_json_filler(p)
         file_count += 1
         pb.update(len(p["data"]["lines"]))

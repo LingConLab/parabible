4ed062992bfefb070975b38f6408bb6b51cd750e
Kesha <affenmilchmann@gmail.com>
2023-03-05 02:48:53 +0300

    rel html hrefs

--- web/dbmanager/_core.py
+++ web/dbmanager/_core.py
@@ -6,7 +6,7 @@ import logging
 
 logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 from ._schema import execute_from_schema
 

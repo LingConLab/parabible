3a861da711af305b850467263e97cd560e01d4e7
Kesha <affenmilchmann@gmail.com>
2023-03-05 03:06:40 +0300

    modified:   web/app/__init__.py

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -2,5 +2,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+app.config['APPLICATION_ROOT'] = "/parabible"
+
 bootstrap = Bootstrap(app)
 from app import views
